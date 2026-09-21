02eb02266782e7e7d578542db02610fdae693f90
Shlok Tiwari <shlok11@somaiya.edu>
2026-09-21 20:46:41 +0530

    [167] [Time Beats: 11.72%] [Memory Beats: 10.36%] - LeetPush

---
+++ TwoPointers/167-Two-Sum-II---Input-Array-Is-Sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n=len(numbers)
+        l=0
+        r=n-1
+        arr=numbers
+        while(l<=r):
+            total=arr[l]+arr[r]
+            if total>target:
+                r-=1
+            if total<target:
+                l+=1
+            if total==target:
+                return [l+1,r+1]
