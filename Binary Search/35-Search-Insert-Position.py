fbab2578f7ceaa485d906a4a76cfe624e3b76a01
Shlok Tiwari <shlok11@somaiya.edu>
2026-09-22 00:06:59 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 12.00%] - LeetPush

---
+++ Binary Search/35-Search-Insert-Position.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        n=len(nums)
+        l=0
+        r=n-1
+       
+        while(l<=r):
+            m=l+(r-l)//2
+            if nums[m]==target:
+                return m
+            elif nums[m]>target:
+                r=m-1
+            elif nums[m]<target:
+                l=m+1
+        if nums[m]<target:
+            return m+1
+        else:
+            return m
